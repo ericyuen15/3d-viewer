6b5d8d51bdc31733debcc7d8e56aa09fb60fd4ac
Eric Yuen <ericyuen15@gmail.com>
2023-10-02 23:55:38 -0700

    main.js re-added

--- main.js
+++ main.js
@@ -12,31 +12,19 @@ function createWindow() {
 
   mainWindow.loadFile('index.html');
 
-  // Open the DevTools (optional)
-  // mainWindow.webContents.openDevTools();
-
   // Dereference the window object when it's closed
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
 }
 
-// This method will be called when Electron has finished initialization
-// and is ready to create browser windows.
 app.on('ready', createWindow);
-
-// Quit when all windows are closed.
 app.on('window-all-closed', function() {
-  // On macOS, applications and their menu bar stay active until the user quits
-  // explicitly with Cmd + Q.
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
-
 app.on('activate', function() {
-  // On macOS, re-create a window in the app when the dock icon is clicked
-  // and there are no other windows open.
   if (mainWindow === null) {
     createWindow();
   }
