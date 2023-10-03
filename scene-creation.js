902c28134a0187d4ce5fac028792f854afa7a2e5
Eric Yuen <ericyuen15@gmail.com>
2023-10-03 00:26:17 -0700

    added .main.js.swp and scene-creation.js

---
+++ scene-creation.js
@@ -0,0 +1,52 @@
+let scene, camera, renderer, cube;
+
+fucntion init() {
+	scene = new THREE,Scene();
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	camera.position.z = 5;
+	
+	renderer = new THREE.WebGLRenderer({ antialias: truealet scene, camera, renderer, cube;
+
+function init() {
+    // Create a new scene
+    scene = new THREE.Scene();
+
+    // Setup the camera
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
+
+    // Create a WebGLRenderer and add to DOM
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    // Create a cube
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    // Handle window resize
+    window.addEventListener('resize', () => {
+        const newWidth = window.innerWidth;
+        const newHeight = window.innerHeight;
+
+        renderer.setSize(newWidth, newHeight);
+        camera.aspect = newWidth / newHeight;
+        camera.updateProjectionMatrix();
+    });
+
+    animate();
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+init();
+
